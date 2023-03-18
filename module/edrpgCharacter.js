ed407428546cb2de573846465f04e4b75aab8307
Daniel Echt <1824222@stud.hs-mannheim.de>
2023-03-18 16:01:42 +0100

    should now log name

--- module/edrpgCharacter.js
+++ module/edrpgCharacter.js
@@ -3,5 +3,13 @@ export default class edrpgCharacter extends Actor {
         super.prepareData();
     }
 
-    
+    getData(){
+        console.log("edrpg | edrpgCharacter | getData");
+        const data = super.getData();
+        if(data.data.system) {
+            data.system = data.data.system;
+        }
+
+        console.log("edrpg | edrpgCharacter | getData | Name is: " + data.system.name);
+    }
 }
