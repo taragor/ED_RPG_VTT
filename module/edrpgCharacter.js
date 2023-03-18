61814b5e670772597a6ef7d2d3aa6ce9ecb731af
Daniel Echt <1824222@stud.hs-mannheim.de>
2023-03-18 16:40:00 +0100

    more testing

--- module/edrpgCharacter.js
+++ module/edrpgCharacter.js
@@ -13,4 +13,25 @@ export default class edrpgCharacter extends Actor {
         console.log("edrpg | edrpgCharacter | getData | Name is: " + data.actor.name);
         return data;
     }
+
+    activateListeners(html) {
+        super.activateListeners(html);
+
+        html.find(".inline_edit").change(this._onInlineEdit.bind(html));
+    }
+
+    _onInlineEdit(event){
+        event.preventDefault();
+        let element = event.currentTarget;
+        let itemId = element.closest(".item").dataset.itemId;
+        let item = this.actor.items.get(itemId);
+        let field = this.element.dataset.field;
+        let value = element.value;
+        if (value == "false") {
+            value = true
+        }
+        if (value == "true" || value == "checked") {
+            value = false
+        }
+    }
 }
